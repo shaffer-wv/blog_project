6122a8ecb03a83d9cf2e88994c8bc9cd4d05a091
Jason Shaffer <shaffer.wv@gmail.com>
2014-09-23 20:21:21 -0400

    Post admin tests in place

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -79,7 +79,84 @@ class AdminTest(LiveServerTestCase):
 		# Check 'Log in' in response
 		self.assertTrue('Log in' in response.content)
 
+	def test_create_post(self):
+		# Log in
+		self.client.login(username='bobsmith', password="password")
 
+		# Check response code
+		response = self.client.get('/admin/blogengine/post/add/')
+		self.assertEquals(response.status_code, 200)
 
+		# Create the new post
+		response = self.client.post('/admin/blogengine/post/add/', {
+			'title': 'My first post',
+			'text': 'This is my first post',
+			'pub_date_0': '2014-9-23',
+			'pub_date_1': '22:00:04'
+			},
+			follow=True)
+		self.assertEquals(response.status_code, 200)
+
+		# Check added successfully
+		self.assertTrue('added successfully' in response.content)
+
+		# Check new post now in database
+		all_posts = Post.objects.all()
+		self.assertEquals(len(all_posts), 1)
+
+	def test_edit_post(self):
+		# Create the post
+		post = Post()
+		post.title = 'My first post'
+		post.text = 'This is my first post'
+		post.pub_date = timezone.now()
+		post.save()
+
+		# Log in
+		self.client.login(username='bobsmith', password="password")
+
+		# Edit the post
+		response = self.client.post('/admin/blogengine/post/1/', {
+			'title': 'My second post',
+			'text': 'This is my second post',
+			'pub_date_0': '2014-9-23',
+			'pub_date_1': '22:00:05',
+			},
+			follow=True)
+		self.assertEquals(response.status_code, 200)
+
+		# Check changed successfully
+		self.assertTrue('changed successfully' in response.content)
+
+		# Check edits are in database
+		all_posts = Post.objects.all()
+		self.assertEquals(len(all_posts), 1)
+		only_post = all_posts[0]
+		self.assertEquals(only_post.title, 'My second post')
+		self.assertEquals(only_post.text, 'This is my second post')
+
+	def test_delete_post(self):
+		# Create the post
+		post = Post()
+		post.title = 'My first post'
+		post.text = 'This is my first post'
+		post.pub_date = timezone.now()
+		post.save()
+
+		# Log in
+		self.client.login(username='bobsmith', password="password")
+
+		# Delete post
+		response = self.client.post('/admin/blogengine/post/1/delete/', {
+			'post': 'yes'
+			}, follow=True)
+		self.assertEquals(response.status_code, 200)
+
+		# Check deleted successfully
+		self.assertTrue('deleted successfully' in response.content)
+
+		# Check post deleted
+		all_posts = Post.objects.all()
+		self.assertEquals(len(all_posts), 0)
 
 
