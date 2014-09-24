ad8e771bb0f767010e2d3f0e93da34c52a4d3b16
Jason Shaffer <shaffer.wv@gmail.com>
2014-09-23 20:37:02 -0400

    Implemented list view for posts

---
+++ blogengine/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from django.views.generic import ListView
+from blogengine.models import Post
+
+urlpatterns = patterns('',
+	# Index
+	url(r'^(?P<page>\d+)?/?$', ListView.as_view(model=Post, paginate_by=5)),	
+)
