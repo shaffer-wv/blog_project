f4330e2473826e3032cfb17585ab9a328e6b1090
Jason Shaffer <shaffer.wv@gmail.com>
2014-10-05 13:54:40 -0400

    Fixed a bug with the Post model

--- blogengine/models.py
+++ blogengine/models.py
@@ -44,7 +44,7 @@ class Post(models.Model):
 	slug = models.SlugField(max_length=40, unique=True)
 	site = models.ForeignKey(Site)
 	category = models.ForeignKey(Category, blank=True, null=True)
-	tags = models.ManyToManyField(Tag)
+	tags = models.ManyToManyField(Tag, blank=True, null=True)
 
 	def get_absolute_url(self):
 		return "/%s/%s/%s/" % (self.pub_date.year, self.pub_date.month, self.slug)
